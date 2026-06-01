4b91d038998d6ad5d7ed1c459880e6e5fca4acaf
Franco Couto <francocouto@gmail.com>
2026-06-01 15:22:30 -0300

    Instanciando a base de dados

---
+++ src/main/java/com/francocouto/projetoMongo/config/testConfig.java
@@ -0,0 +1,31 @@
+package com.francocouto.projetoMongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.francocouto.projetoMongo.domain.User;
+import com.francocouto.projetoMongo.repositorios.UserRepositorio;
+
+@Configuration
+public class testConfig implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepositorio userRepositorio;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepositorio.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
+		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
+		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
+		
+		userRepositorio.saveAll(Arrays.asList(maria, alex , bob));
+		
+	}
+
+}
